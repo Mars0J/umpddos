57ee98ecd0192020636641f33191eefa0e7db6ec
Mars0J <70023143+Mars0J@users.noreply.github.com>
2021-02-09 18:35:21 +0300

    Create setup.rb

---
+++ setup.rb
@@ -0,0 +1,6 @@
+puts "\033[33mBaşlıyor...."
+system("clear")
+system("pip3 install termcolor")
+system("pip3 install colorama")
+system("pip3 install progress")
+system("pip3 install alive-progress")
